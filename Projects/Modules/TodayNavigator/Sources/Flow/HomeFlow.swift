2bf82e11c46b873c90aa76f662bb42d7666184d6
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-01 19:46:27 +0900

    안쓰는 플로우 삭제

--- Projects/Modules/TodayNavigator/Sources/Flow/HomeFlow.swift
+++ Projects/Modules/TodayNavigator/Sources/Flow/HomeFlow.swift
@@ -6,39 +6,39 @@
 //  Copyright © 2023 APP-iOS. All rights reserved.
 //
 
-import Foundation
-import RxFlow
-import UIKit
-import Core
-import Presentation
+//import Foundation
+//import RxFlow
+//import UIKit
+//import Core
+//import Presentation
+//
+//class HomeFlow: Flow {
+//
+//    var root: Presentable {
+//        return self.rootViewController
+//    }
+//
+//    private lazy var rootViewController: UINavigationController = {
+//        let viewController = UINavigationController()
+//        return viewController
+//    }()
+//
+//    init() {}
 
-class HomeFlow: Flow {
-
-    var root: Presentable {
-        return self.rootViewController
-    }
-
-    private lazy var rootViewController: UINavigationController = {
-        let viewController = UINavigationController()
-        return viewController
-    }()
-
-    init() {}
-
-    func navigate(to step: Step) -> FlowContributors {
-        guard let step = step as? AppStep else { return .none }
-        switch step {
-        case .homeIsRequired:
-            return self.navigateToHome()
-        case .loginIsRequired:
-            return .end(forwardToParentFlowWithStep: AppStep.loginIsRequired)
-        }
-    }
-    
-    private func navigateToHome() -> FlowContributors {
-        let viewController = HomeViewController()
-        self.rootViewController.setViewControllers([viewController], animated: false)
-        return .one(flowContributor: .contribute(withNext: viewController))
-    }
-}
+//    func navigate(to step: Step) -> FlowContributors {
+//        guard let step = step as? AppStep else { return .none }
+//        switch step {
+//        case .homeIsRequired:
+//            return self.navigateToHome()
+//        case .loginIsRequired:
+//            return .end(forwardToParentFlowWithStep: AppStep.loginIsRequired)
+//        }
+//    }
+//    
+//    private func navigateToHome() -> FlowContributors {
+//        let viewController = HomeViewController()
+//        self.rootViewController.setViewControllers([viewController], animated: false)
+//        return .one(flowContributor: .contribute(withNext: viewController))
+//    }
+//}
 
